af0436a4f78a324d700b5e5f2995e965772ca190
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-30 00:06:57 +0530

    chore: dependency injection

--- di/wire.go
+++ di/wire.go
@@ -4,16 +4,37 @@ package di
 
 import (
 	"github.com/google/wire"
+	driverrepository "github.com/ride-app/marketplace-service/repositories/driver"
+	locationrepository "github.com/ride-app/marketplace-service/repositories/location"
+	statusrepository "github.com/ride-app/marketplace-service/repositories/status"
+	walletrepository "github.com/ride-app/marketplace-service/repositories/wallet"
 	"github.com/ride-app/marketplace-service/service"
+	thirdparty "github.com/ride-app/marketplace-service/third-party"
 )
 
-func InitializeService() (*service.EntityServiceServer, error) {
+func InitializeService() (*service.MarketplaceServiceServer, error) {
 	panic(
 		wire.Build(
-			statusRepository.NewFirebaseStatusRepository,
+			thirdparty.NewFirebaseApp,
+			statusrepository.NewFirebaseStatusRepository,
 			wire.Bind(
-				new(statusRepository.StatusRepository),
-				new(*statusRepository.FirebaseImpl),
+				new(statusrepository.StatusRepository),
+				new(*statusrepository.FirebaseImpl),
+			),
+			locationrepository.NewFirebaseLocationRepository,
+			wire.Bind(
+				new(locationrepository.LocationRepository),
+				new(*locationrepository.FirebaseImpl),
+			),
+			driverrepository.New,
+			wire.Bind(
+				new(driverrepository.DriverRepository),
+				new(*driverrepository.Impl),
+			),
+			walletrepository.New,
+			wire.Bind(
+				new(walletrepository.WalletRepository),
+				new(*walletrepository.Impl),
 			),
 			service.New,
 		),
