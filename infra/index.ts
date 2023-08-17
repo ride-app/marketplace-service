602b690a1aa0ecc7392c4f18a78c66627560a12a
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-17 21:35:56 +0530

    ci: migrate to cloud build

---
+++ infra/index.ts
@@ -0,0 +1,34 @@
+import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
+
+const serviceName =
+  new pulumi.Config("service").get("name") || pulumi.getProject();
+const location = gcp.config.region || "asia-east1";
+
+const github_connection = gcp.cloudbuildv2.Connection.get(
+  "github-connection",
+  pulumi.interpolate`projects/${gcp.config.project}/locations/${location}/connections/GitHub`,
+);
+
+const repository = new gcp.cloudbuildv2.Repository("repository", {
+  location,
+  name: serviceName,
+  parentConnection: github_connection.name,
+  remoteUri: pulumi.interpolate`https://github.com/ride-app/${serviceName}.git`,
+});
+
+new gcp.cloudbuild.Trigger("build-trigger", {
+  name: serviceName,
+  location,
+  repositoryEventConfig: {
+    repository: repository.id,
+    push: {
+      branch: "^main$",
+    },
+  },
+  substitutions: {
+    _LOG_DEBUG: new pulumi.Config().get("logDebug") ?? "false",
+  },
+  filename: "cloudbuild.yaml",
+  includeBuildLogs: "INCLUDE_BUILD_LOGS_WITH_STATUS",
+});
