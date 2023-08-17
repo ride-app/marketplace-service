b67f5149cbd1efb80707b09c7178caf507a36908
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-17 21:37:30 +0530

    refactor: refactor config

--- config/config.go
+++ config/config.go
@@ -1,11 +1,12 @@
 package config
 
 type ConfigStruct struct {
-	Debug               bool   `env:"DEBUG" env-description:"dev or prod" env-default:"false"`
+	Production          bool   `env:"PRODUCTION" env-description:"dev or prod" env-default:"true"`
+	LogDebug            bool   `env:"LOG_DEBUG" env-description:"should log at debug level" env-default:"false"`
 	Port                int32  `env:"PORT" env-description:"server port" env-default:"50051"`
 	Wallet_Service_Host string `env:"WALLET_SERVICE_HOST" env-description:"wallet service host" env-default:"localhost:50052"`
 	Driver_Service_Host string `env:"DRIVER_SERVICE_HOST" env-description:"wallet service host" env-default:"localhost:50052"`
-	Firebase_Project_Id string `env:"FIREBASE_PROJECT_ID" env-description:"firebase project id" env-default:"NO_PROJECT"`
+	Project_Id          string `env:"PROJECT_ID" env-description:"firebase project id" env-default:"NO_PROJECT"`
 }
 
 var Env ConfigStruct
