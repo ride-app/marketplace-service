9766636b166f3933ee6550eb532738d75511d480
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-24 13:54:06 +0530

    feat: watch location

--- repositories/location/location_repository.go
+++ repositories/location/location_repository.go
@@ -18,6 +18,8 @@ import (
 type LocationRepository interface {
 	GetLocation(ctx context.Context, log logger.Logger, id string) (*pb.Location, error)
 
+	ListenLocation(ctx context.Context, log logger.Logger, id string, locationResponseStream chan<- *LocationStreamResponse)
+
 	UpdateLocation(ctx context.Context, log logger.Logger, id string, location *pb.Location) (updateTime *time.Time, err error)
 }
 
@@ -71,6 +73,65 @@ func (r *FirebaseImpl) GetLocation(ctx context.Context, log logger.Logger, id st
 		Latitude:  latitude,
 		Longitude: longitude,
 	}, nil
+}
+
+type LocationStreamResponse struct {
+	Location *pb.Location
+	Error    error
+}
+
+func (r *FirebaseImpl) ListenLocation(ctx context.Context, log logger.Logger, id string, locationResponseStream chan<- *LocationStreamResponse) {
+	log.Info("Listening for driver location changes in firestore")
+	snapshots := r.firestore.Collection("activeDrivers").Doc(id).Snapshots(ctx)
+
+	defer snapshots.Stop()
+
+	for {
+		snapshot, err := snapshots.Next()
+
+		if status.Code(err) == codes.DeadlineExceeded {
+			log.Info("firestore deadline exceeded")
+			locationResponseStream <- &LocationStreamResponse{
+				Location: nil,
+				Error:    nil,
+			}
+		} else if status.Code(err) == codes.NotFound {
+			log.Info("Driver does not exist in firestore")
+			locationResponseStream <- &LocationStreamResponse{
+				Location: nil,
+				Error:    nil,
+			}
+		} else if err != nil {
+			log.WithError(err).Error("Error listening for driver location changes from firestore")
+			locationResponseStream <- &LocationStreamResponse{
+				Location: nil,
+				Error:    err,
+			}
+		}
+
+		if !snapshot.Exists() {
+			log.Info("Driver location not found in firestore")
+			locationResponseStream <- &LocationStreamResponse{
+				Location: nil,
+				Error:    nil,
+			}
+			return
+		}
+
+		data := snapshot.Data()
+
+		location := data["location"].(map[string]interface{})
+		latitude := location["latitude"].(float64)
+		longitude := location["longitude"].(float64)
+
+		locationResponseStream <- &LocationStreamResponse{
+			Location: &pb.Location{
+				Latitude:  latitude,
+				Longitude: longitude,
+			},
+			Error: nil,
+		}
+	}
 }
 
 func (r *FirebaseImpl) UpdateLocation(ctx context.Context, log logger.Logger, id string, location *pb.Location) (updateTime *time.Time, err error) {
